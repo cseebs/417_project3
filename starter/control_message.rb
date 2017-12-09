4d9ec5c8ad5eee51216a5478f11edc495db5fc50
biscut14 <doubleb96@msn.com>
2017-12-08 18:51:38 -0800

    Update control_message.rb

--- starter/control_message.rb
+++ starter/control_message.rb
@@ -8,6 +8,7 @@ module Ctrl
 		if (message.length >= Message::HEADER_LENGTH + 1)
 			$sync.synchronize {
 				msg = Message.new(message.chop)
+				STDOUT.puts(msg.toString())
 				seq = msg.getField("frag_seq")
 				num = msg.getField("frag_num")
 				if (seq == 0) 
@@ -33,9 +34,10 @@ module Ctrl
 
 	def Ctrl.handle(msg, client)
 		type = msg.getField("type")
+		STDOUT.puts(type)
 		if (type == 0) 
 			Ctrl.edgeb(msg, client)
-		elsif (type == 1)
+		else
 			Ctrl.handleFlood(msg, client)
 		end
 	end
@@ -46,37 +48,37 @@ module Ctrl
 		dstip = msg[1]
 		dst = msg[2]
 		$routing_table[dst] = [$hostname, dst, dst, 1]
-		$socketToNode[dst] = client
+		$socketToNode[client] = dst
 	end
 
 	def Ctrl.flood()
 		msg = Message.new
-		msg.setField("type", 1)
 		msg.setField("seq_num", $seq_num)
 		$seq_num += 1
-		message = $hostname + " "
+		message = $hostname + "\t"
 		if ($routing_table.length > 0)
 			$routing_table.each do |key, value|
 				dist = value[3]
-				message += key + "," + dist.to_s + " "
+				message += key + "," + dist.to_s + "\t"
 			end
 			msg.setPayload(message)
 			$socketToNode.each do |key, value|
-				Ctrl.sendMsg(msg, value)
+				Ctrl.sendMsg(msg, key)
 			end
 		end
 	end
 
 	def Ctrl.handleFlood(msg, client)
+		STDOUT.puts("Here")
 		num = msg.getField("seq_num")
-		payload_list = msg.getPayload.split(" ")
+		payload_list = msg.getPayload.split("\t")
 		curr_node = payload_list[0]
 		if ($flood_table[curr_node] == nil or 
 			num > $flood_table[curr_node])
 
 			$flood_table[curr_node] = num
 			$socketToNode.each do |key, value|
-				Ctrl.sendMsg(msg, value)
+				Ctrl.sendMsg(msg, key)
 			end
 			dist_table = Hash.new()
 			for index in 1..(payload_list.length - 1)
@@ -108,9 +110,10 @@ module Ctrl
 	end
 
 	def Ctrl.sendMsg(msg, client)
-		list = msg.fragment()
-		list.each do |packet|
+		#list = msg.fragment()
+		#list.each do |packet|
+			STDOUT.puts(msg.toString())
 			client.puts msg.toString()
-		end
+		#end
 	end
-end
+end	
