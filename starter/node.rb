a2eb7ad5927df3e42ed8af7f5e55c68499c6b2d6
cseebs <c.sieber27@gmail.com>
2017-12-08 17:32:25 -0800

    part2 node.rb with status

--- starter/node.rb
+++ starter/node.rb
@@ -16,53 +16,56 @@ $curr_seq = nil
 $flood_table = Hash.new()
 $buffer = []
 $seq_num = 1
+$neighbors = []
 
 
 
 # --------------------- Part 1 --------------------- # 
 
 def edgeb(cmd)
-	src_ip = cmd[0]
-	dst_ip = cmd[1]
-	dst = cmd[2]
-	port = $port_table[dst]
-	sock = TCPSocket.open(dst_ip, port)
-	if sock != nil
-		$routing_table[dst] = [$hostname, dst, dst, 1]
-		$socketToNode[dst] = sock
-		msg = Message.new
-		msg.setField("type", 0)
-		msg.setPayload(dst_ip + "," + src_ip + "," + $hostname)
-		Ctrl.sendMsg(msg, sock)
-	end
+  src_ip = cmd[0]
+  dst_ip = cmd[1]
+  dst = cmd[2]
+  port = $port_table[dst]
+  sock = TCPSocket.open(dst_ip, port)
+  if sock != nil
+    $routing_table[dst] = [$hostname, dst, dst, 1]
+    $socketToNode[dst] = sock
+    msg = Message.new
+    msg.setField("type", 0)
+    msg.setPayload(dst_ip + "," + src_ip + "," + $hostname)
+    Ctrl.sendMsg(msg, sock)
+    neighbors.push(dst)
+  end
 end
 
 def dumptable(cmd)
-	file_name = cmd[0]
+  file_name = cmd[0]
 
-	begin
-		file = File.open(file_name, "w")
+  begin
+    file = File.open(file_name, "w")
 
-	rescue
-		new_file = File.new(file_name)
-		file = File.open(file_name)
-	end
-	$routing_table.each {|key, value| 
-		file.write("#{value[0]},#{value[1]},#{value[2]},#{value[3]}\n")}
-	file.close
+  rescue
+    new_file = File.new(file_name)
+    file = File.open(file_name)
+  end
+
+  $routing_table.each {|key, value| 
+    file.write("#{value[0]},#{value[1]},#{value[2]},#{value[3]}\n")}
+  file.close
 end
 
 def shutdown(cmd)
-   if $server != nil
-	$server.close
-   end
-   $socketToNode.keys.each { |sock|
-     	sock.flush
-    	sock.close
-   }
-   STDOUT.flush
-   STDERR.flush
-   exit(0)
+  if $server != nil
+    $server.close
+  end
+  $socketToNode.values.each { |sock|
+    sock.flush
+    sock.close
+  }
+  STDOUT.flush
+  STDERR.flush
+  exit(0)
 end
 
 
@@ -86,7 +89,12 @@ def edgeu(cmd)
 end
 
 def status()
-	STDOUT.puts "SENDMSG: not implemented"
+  STDOUT.puts "Name: #{$hostname}\nPort: #{$port}\nNeighbors: "
+  neighbors.each_with_index do | value, index |
+    if index == neighbors.length - 1
+      STDOUT.puts " #{value}\n"
+    else
+      STDOUT.puts " #{value},"
 end
 
 
